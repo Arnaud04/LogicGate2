a8502eb1ba82503975dd5fae2ac696120fbac052
firefoxyte <36266609+firefoxyte@users.noreply.github.com>
2018-10-20 23:58:27 +0200

    debut de drag
    peut drag un un gameobject ( testé uniquement sur ordi)
    dorian

---
+++ logicgate2/Assets/Script/Script niveaux/dragging.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class dragging : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    float time;
+    Vector2 pos;
+    // Use this for initialization
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        pos = transform.position;
+        time = Time.realtimeSinceStartup;
+       
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        if( (Time.realtimeSinceStartup - time) < 0.5)
+        {
+            this.transform.position = pos;
+        }
+
+    }
+}
